af103d3d538dd5f70f3d45cc440d2eaf75a75836
Lucas Felipe Costa da Mata <lucasfelipecdm@gmail.com>
2019-07-31 20:24:38 -0300

    initial project commit

---
+++ src/mapa/MAPA.java
@@ -0,0 +1,12 @@
+package mapa;
+/**
+ *
+ * @author Lucas Felipe Costa da Mata
+ */
+public class MAPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
